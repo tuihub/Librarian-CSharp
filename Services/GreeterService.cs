7e1c1e4dfa8c0aeb31367a46818a00354806d157
Tabing010102 <tabing010102@outlook.com>
2022-07-27 21:59:31 +0800

    add async test

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -13,9 +13,19 @@ namespace Librarian_CSharp.Services
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
+            Task.Delay(2000).Wait();
             return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
+            });
+        }
+
+        public override Task<HelloReply2> SayHello2(HelloRequest2 request, ServerCallContext context)
+        {
+            Task.Delay(3000).Wait();
+            return Task.FromResult(new HelloReply2
+            {
+                Message = "Hello2 " + request.Name
             });
         }
     }
