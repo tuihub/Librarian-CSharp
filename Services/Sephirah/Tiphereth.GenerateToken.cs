30e0a12d0728c09d7b0aae4bf283ab7694a775f5
Tabing010102 <tabing010102@outlook.com>
2022-10-15 20:18:33 +0800

    impl Tiphereth.GenerateToken

---
+++ Services/Sephirah/Tiphereth.GenerateToken.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using Librarian.Utils;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+
+namespace Librarian.Services.Sephirah
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        public override Task<GenerateTokenResponse> GenerateToken(GenerateTokenRequest request, ServerCallContext context)
+        {
+            string refreshToken;
+            try
+            {
+                using var db = new TestDbContext();
+                // verify user type(admin)
+                var token = context.RequestHeaders.Single(x => x.Key == "authorization").Value;
+                var internalIdFromToken = JwtUtil.GetInternalIdFromToken(token);
+                var userFromToken = db.Users.Single(x => x.InternalId == internalIdFromToken);
+                if (userFromToken.Type != UserType.Admin)
+                    throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
+                // generate token
+                var internalId = request.Id.Id;
+                var user = db.Users.SingleOrDefault(x => x.InternalId == internalId);
+                if (user == null)
+                    throw new RpcException(new Status(StatusCode.InvalidArgument, "User not exists."));
+                refreshToken = JwtUtil.GenerateRefreshToken(internalId);
+            }
+            catch (RpcException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new RpcException(new Status(StatusCode.Unavailable, e.Message));
+            }
+            return Task.FromResult(new GenerateTokenResponse()
+            {
+                RefreshToken = refreshToken
+            });
+        }
+    }
+}
