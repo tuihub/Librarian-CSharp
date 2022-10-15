064dfa4460ffd0193edffcd6924bffa0cf5ea3a1
Tabing010102 <tabing010102@outlook.com>
2022-10-15 20:10:50 +0800

    impl Tiphereth.UpdateUser

---
+++ Services/Sephirah/Tiphereth.UpdateUser.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Librarian.Utils;
+using Microsoft.AspNetCore.Authorization;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+
+namespace Librarian.Services.Sephirah
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        [Authorize]
+        public override Task<UpdateUserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                using var db = new TestDbContext();
+                // verify user type(admin)
+                var token = context.RequestHeaders.Single(x => x.Key == "authorization").Value;
+                var internalIdFromToken = JwtUtil.GetInternalIdFromToken(token);
+                var userFromToken = db.Users.Single(x => x.InternalId == internalIdFromToken);
+                if (userFromToken.Type != UserType.Admin)
+                    throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
+                // update user
+                var userReq = request.User;
+                var user = db.Users.SingleOrDefault(x => x.InternalId == userReq.Id.Id);
+                if (user == null)
+                    throw new RpcException(new Status(StatusCode.InvalidArgument, "User not exists."));
+                if (userReq.Username.Length > 0) user.Name = userReq.Username;
+                if (userReq.Password.Length > 0) user.Password = PasswordHasher.HashPassword(userReq.Password);
+                if (userReq.Type != UserType.Unspecified) user.Type = userReq.Type;
+                if (userReq.Status != UserStatus.Unspecified) user.Status = userReq.Status;
+                db.SaveChanges();
+            }
+            catch (RpcException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new RpcException(new Status(StatusCode.Unavailable, e.Message));
+            }
+            return Task.FromResult(new UpdateUserResponse());
+        }
+    }
+}
