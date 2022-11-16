b762edf9ca0ef0e4df19829f8d403598f5d70907
Tabing010102 <tabing010102@outlook.com>
2022-11-16 21:16:44 +0800

    update .gitignore & add test

---
+++ LibrarianTests/IntegrationTests/Services/Sephirah/SephirahBase.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarianTests.IntegrationTests.Services.Sephirah
+{
+    public class SephirahBase
+    {
+        private GrpcChannel? _channel;
+
+        protected GrpcChannel Channel => _channel ??= CreateChannel();
+
+        protected GrpcChannel CreateChannel()
+        {
+            return GrpcChannel.ForAddress(GlobalContext.SephirahServiceAddr);
+        }
+    }
+}
---
+++ LibrarianTests/IntegrationTests/Services/Sephirah/Tiphereth/GetTokenTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using Xunit;
+using LibrarianTests.IntegrationTests.Services.Sephirah;
+using Grpc.Core;
+
+namespace LibrarianTests.Services.Sephirah.Tiphereth
+{
+    public partial class Tiphereth : SephirahBase
+    {
+        [Fact]
+        public async Task Test_UsernameOrPasswordError()
+        {
+            var client = new LibrarianSephirahService.LibrarianSephirahServiceClient(Channel);
+
+            var usernames = new[] { "test1", "test", "test1" };
+            var passwords = new[] { "test", "test1", "test1" };
+            var exception = StatusCode.PermissionDenied;
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                var u = usernames[i];
+                var p = passwords[i];
+
+                var ex = Assert.ThrowsException<RpcException>(() =>
+                {
+                    var response = client.GetToken(new GetTokenRequest
+                    {
+                        Username = u,
+                        Password = p,
+                    });
+                });
+                Assert.Equals(ex.StatusCode, exception);
+            }
+        }
+    }
+}
