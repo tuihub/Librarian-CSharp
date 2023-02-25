bf3d348f0d2b5764b44605958b599e95d084a494
Tabing010102 <tabing010102@outlook.com>
2023-02-25 11:36:40 +0800

    update App & AppDetails model

--- Librarian/Models/App.cs
+++ Librarian/Models/App.cs
@@ -14,5 +14,17 @@ namespace Librarian.Models
         public string? ShortDescription { get; set; }
         public string? ImageUrl { get; set; }
         public AppDetails? AppDetails { get; set; }
+        public App(long internalId, TuiHub.Protos.Librarian.V1.App app)
+        {
+            InternalId = internalId;
+            Source = app.Source;
+            SourceAppId = app.SourceAppId;
+            SourceUrl = app.SourceUrl;
+            Name = app.Name;
+            Type = app.Type;
+            ShortDescription = app.ShortDescription;
+            ImageUrl = app.ImageUrl;
+            AppDetails = AppDetails.FromProtosAppDetails(app.Details);
+        }
     }
 }
