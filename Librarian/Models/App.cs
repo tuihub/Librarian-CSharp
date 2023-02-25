fc6672f92f3be9dafaf1743e48facee8c4b0f368
Tabing010102 <tabing010102@outlook.com>
2023-02-25 10:44:42 +0800

    add App model

---
+++ Librarian/Models/App.cs
@@ -0,0 +1,18 @@
+﻿using TuiHub.Protos.Librarian.V1;
+
+namespace Librarian.Models
+{
+    public class App
+    {
+        public long Id { get; set; }
+        public long InternalId { get; set; }
+        public AppSource Source { get; set; }
+        public string SourceAppId { get; set; } = null!;
+        public string? SourceUrl { get; set; }
+        public string Name { get; set; } = null!;
+        public AppType Type { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ImageUrl { get; set; }
+        public AppDetails? AppDetails { get; set; }
+    }
+}
---
+++ Librarian/Models/AppDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Librarian.Models
+{
+    public class AppDetails
+    {
+        public string? Description { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Developer { get; set; }
+        public string? Publisher { get; set; }
+        public string? Version { get; set; }
+    }
+}
