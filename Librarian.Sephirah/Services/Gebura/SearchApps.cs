6e0ce4813cbd6790593414c89391f596bfdf5777
Tabing010102 <tabing010102@outlook.com>
2023-05-05 16:18:26 +0800

    add Gebura.SearchApps

---
+++ Librarian.Sephirah/Services/Gebura/SearchApps.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Librarian.Sephirah.Utils;
+using Microsoft.AspNetCore.Authorization;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using TuiHub.Protos.Librarian.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        [Authorize]
+        public override Task<SearchAppsResponse> SearchApps(SearchAppsRequest request, ServerCallContext context)
+        {
+            using var db = new TestDbContext();
+            // get request param
+            var pageNum = request.Paging.PageNum;
+            var pageSize = request.Paging.PageSize;
+            string keyword = request.Keywords;
+            // filter apps
+            IEnumerable<Models.App> apps = db.Apps;
+            apps = apps.Where(a => a.Name.Contains(keyword));
+            apps = apps.Skip(pageSize * (pageNum - 1)).Take(pageSize);
+            // construct response
+            var response = new SearchAppsResponse
+            {
+                Paging = new TuiHub.Protos.Librarian.V1.PagingResponse { TotalSize = apps.Count() }
+            };
+            response.Apps.Add(apps.Select(x => x.ToProtoApp()));
+            return Task.FromResult(response);
+        }
+    }
+}
