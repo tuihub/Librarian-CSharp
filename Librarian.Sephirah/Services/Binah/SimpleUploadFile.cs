50266683d183971df170a1c12a82349d3d7292cf
Tabing010102 <tabing010102@outlook.com>
2023-05-26 14:49:37 +0800

    fix SimpleUploadFile

--- Librarian.Sephirah/Services/Binah/SimpleUploadFile.cs
+++ Librarian.Sephirah/Services/Binah/SimpleUploadFile.cs
@@ -3,6 +3,7 @@ using Librarian.Sephirah.Models;
 using Librarian.Sephirah.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Minio;
+using System.Diagnostics;
 using System.IO.Pipes;
 using System.Security.Cryptography;
 using TuiHub.Protos.Librarian.Sephirah.V1;
@@ -27,22 +28,27 @@ namespace Librarian.Sephirah.Services
             var pipeStreamServer4Minio = new AnonymousPipeServerStream();
             var pipeStreamClient4Minio = new AnonymousPipeClientStream(pipeStreamServer4Minio.GetClientHandleAsString());
             //using var memoryStream = new MemoryStream();
-            long receivedBytes = 0;
+            //long receivedBytes = 0;
+            Debug.WriteLine($"SimpleUploadFile: Starting readTask");
             var readTask = Task.Run(async () =>
             {
                 gameSaveFile.Status = GameSaveFileStatus.IN_PROGRESS;
                 await foreach (var message in requestStream.ReadAllAsync())
                 {
+                    Debug.WriteLine($"SimpleUploadFile: readTask message.Data.Length = {message.Data?.Length}");
                     if (message.Data != null)
                     {
                         //await memoryStream.WriteAsync(message.Data.Memory);
                         await pipeStreamServer4Sha256.WriteAsync(message.Data.Memory);
                         await pipeStreamServer4Minio.WriteAsync(message.Data.Memory);
-                        receivedBytes += message.Data.Length;
+                        //receivedBytes += message.Data.Length;
                     }
                 }
+                pipeStreamServer4Sha256.Close();
+                pipeStreamServer4Minio.Close();
             });
             var writeTaskCancellationTokenSource = new CancellationTokenSource();
+            Debug.WriteLine($"SimpleUploadFile: Starting writeTask");
             var writeTask = Task.Run(async () =>
             {
                 long executionNum = 0;
@@ -71,10 +77,13 @@ namespace Librarian.Sephirah.Services
             var putObjectArgs = new PutObjectArgs()
                                     .WithBucket(GlobalContext.SystemConfig.MinioBucket)
                                     .WithObject(internalId.ToString())
-                                    .WithStreamData(pipeStreamClient4Minio);
+                                    .WithStreamData(pipeStreamClient4Minio)
+                                    .WithObjectSize(fileMetadata.Size);
+            Debug.WriteLine($"SimpleUploadFile: Starting minioPutTask");
             var minioPutTask = minioClient.PutObjectAsync(putObjectArgs);
             // calc Sha256
             using var sha256 = SHA256.Create();
+            Debug.WriteLine($"SimpleUploadFile: Starting sha256Task");
             var sha256Task = sha256.ComputeHashAsync(pipeStreamClient4Sha256);
             // wait for readTask, minioPutTask, sha256Task
             Task.WaitAll(readTask, minioPutTask, sha256Task);
