da3dc9129788a9c99eb8222e02c3035726df91ed
Tabing010102 <tabing010102@outlook.com>
2023-05-30 16:18:03 +0800

    impl Pin & Unpin GameSaveFile

---
+++ Librarian.Sephirah/Services/Gebura/UnpinGameSaveFile.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Librarian.Sephirah.Models;
+using Librarian.Sephirah.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Minio;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using TuiHub.Protos.Librarian.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        [Authorize]
+        public override Task<UnpinGameSaveFileResponse> UnpinGameSaveFile(UnpinGameSaveFileRequest request, ServerCallContext context)
+        {
+            var id = request.Id.Id;
+            var gameSaveFile = _dbContext.GameSaveFiles.Single(x => x.Id == id);
+            gameSaveFile.IsPinned = false;
+            _dbContext.SaveChanges();
+            return Task.FromResult(new UnpinGameSaveFileResponse());
+        }
+    }
+}
