25fcd04fc63d4e03dd839fbc538c789df0e9206e
Tabing010102 <tabing010102@outlook.com>
2023-05-30 16:19:03 +0800

    impl SetUserSaveFileCapacity

---
+++ Librarian.Sephirah/Services/Gebura/SetUserSaveFileCapacity.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using Librarian.Sephirah.Utils;
+using Microsoft.AspNetCore.Authorization;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using TuiHub.Protos.Librarian.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        [Authorize]
+        public override Task<SetUserSaveFileCapacityResponse> SetUserSaveFileCapacity(SetUserSaveFileCapacityRequest request, ServerCallContext context)
+        {
+            // verify user type(admin)
+            if (UserUtil.GetUserTypeFromToken(context, _dbContext) != UserType.Admin)
+                throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
+            // set capacity
+            var userId = request.UserId.Id;
+            long? capacityBytes = request.HasCapacityBytes ? request.CapacityBytes : null;
+            var user = _dbContext.Users.Single(x => x.Id == userId);
+            user.GameSaveFileCapacityBytes = capacityBytes;
+            _dbContext.SaveChanges();
+            return Task.FromResult(new SetUserSaveFileCapacityResponse());
+        }
+    }
+}
