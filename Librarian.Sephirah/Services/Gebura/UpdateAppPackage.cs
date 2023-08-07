f52cec0bfc930312de87f64297bd35fc897acdbf
Tabing010102 <tabing010102@outlook.com>
2023-08-07 17:14:45 +0800

    impl Gebura.UpdateAppPackage

---
+++ Librarian.Sephirah/Services/Gebura/UpdateAppPackage.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Librarian.Common.Utils;
+using Microsoft.AspNetCore.Authorization;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using TuiHub.Protos.Librarian.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        [Authorize]
+        public override Task<UpdateAppPackageResponse> UpdateAppPackage(UpdateAppPackageRequest request, ServerCallContext context)
+        {
+            // verify user type(admin)
+            if (UserUtil.GetUserTypeFromJwt(context, _dbContext) != UserType.Admin)
+                throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
+            // check AppPackage exists
+            var appPackageReq = request.AppPackage;
+            var appPackage = _dbContext.AppPackages.SingleOrDefault(x => x.Id == appPackageReq.Id.Id);
+            if (appPackage == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "User not exists."));
+            // update AppPackage
+            appPackage.Source = appPackageReq.Source;
+            appPackage.SourceAppId = appPackageReq.SourceId.Id;
+            appPackage.Name = appPackageReq.Name;
+            appPackage.Description = appPackageReq.Description;
+            appPackage.IsPublic = appPackageReq.Public;
+            appPackage.AppPackageBinary = appPackage.AppPackageBinary;
+            appPackage.UpdatedAt = DateTime.Now;
+            _dbContext.SaveChanges();
+            return Task.FromResult(new UpdateAppPackageResponse());
+        }
+    }
+}
