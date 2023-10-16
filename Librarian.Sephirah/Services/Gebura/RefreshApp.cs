5fe2df04b9616eab2e222161b4bde5e13faf763b
Tabing010102 <tabing010102@outlook.com>
2023-10-16 22:59:39 +0800

    update Gebura.RefreshApp

--- Librarian.Sephirah/Services/Gebura/RefreshApp.cs
+++ Librarian.Sephirah/Services/Gebura/RefreshApp.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Core;
 using Librarian.Common.Utils;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using TuiHub.Protos.Librarian.Sephirah.V1;
 
 namespace Librarian.Sephirah.Services
@@ -15,11 +16,16 @@ namespace Librarian.Sephirah.Services
                 throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
             // get request param
             var appId = request.AppId.Id;
-            var app = _dbContext.Apps.Single(x => x.Id == appId);
-            if (app.Source == TuiHub.Protos.Librarian.V1.AppSource.Internal)
-                throw new RpcException(new Status(StatusCode.InvalidArgument, "Cannot refresh internal app."));
+            var app = _dbContext.Apps.Include(x => x.ChildApps)
+                                     .Single(x => x.Id == appId);
+            if (app.Source != TuiHub.Protos.Librarian.V1.AppSource.Internal)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Cannot refresh not internal app."));
             // refresh app
-            _pullMetadataService.AddPullApp(appId);
+            foreach (var childApp in app.ChildApps)
+                if (childApp.Source == TuiHub.Protos.Librarian.V1.AppSource.Steam ||
+                    childApp.Source == TuiHub.Protos.Librarian.V1.AppSource.Vndb ||
+                    childApp.Source == TuiHub.Protos.Librarian.V1.AppSource.Bangumi)
+                    _pullMetadataService.AddPullApp(childApp.Id);
             return Task.FromResult(new RefreshAppResponse { });
         }
     }
