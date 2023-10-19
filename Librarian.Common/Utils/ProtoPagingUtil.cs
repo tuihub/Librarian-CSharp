d4f215682797e5d2eab535a6d21d11e3c7d2a946
Tabing010102 <tabing010102@outlook.com>
2023-10-19 13:15:02 +0800

    fix: handle null PagingRequest

--- Librarian.Common/Utils/ProtoPagingUtil.cs
+++ Librarian.Common/Utils/ProtoPagingUtil.cs
@@ -4,11 +4,15 @@ namespace Librarian.Common.Utils
 {
     public static class ProtoPagingUtil
     {
-        public static IQueryable<T> ApplyPagingRequest<T>(this IQueryable<T> source, PagingRequest pagingRequest)
+        public static IQueryable<T> ApplyPagingRequest<T>(this IQueryable<T> source, PagingRequest? pagingRequest)
         {
-            var pageSize = pagingRequest.PageSize;
-            var pageNum = pagingRequest.PageNum;
-            return source.Skip(pageSize * (pageNum - 1)).Take(pageSize);
+            if (pagingRequest == null) return source;
+            else
+            {
+                var pageSize = pagingRequest.PageSize;
+                var pageNum = pagingRequest.PageNum;
+                return source.Skip(pageSize * (pageNum - 1)).Take(pageSize);
+            }
         }
     }
 }
