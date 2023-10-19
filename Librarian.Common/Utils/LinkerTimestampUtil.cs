c63e811d087e4999b816f54fc52949aef900905b
Tabing010102 <tabing010102@outlook.com>
2023-10-19 13:14:20 +0800

    feat(Sephirah): impl GetServerInformation

---
+++ Librarian.Common/Utils/LinkerTimestampUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarian.Common.Utils
+{
+    // from https://www.meziantou.net/getting-the-date-of-build-of-a-dotnet-assembly-at-runtime.htm
+    public static class LinkerTimestampUtil
+    {
+        public static DateTime GetLinkerTimestamp(Assembly assembly)
+        {
+            var location = assembly.Location;
+            return GetLinkerTimestampUtc(location).ToLocalTime();
+        }
+
+        public static DateTime GetLinkerTimestampUtc(Assembly assembly)
+        {
+            var location = assembly.Location;
+            return GetLinkerTimestampUtc(location);
+        }
+
+        public static DateTime GetLinkerTimestampUtc(string filePath)
+        {
+            const int peHeaderOffset = 60;
+            const int linkerTimestampOffset = 8;
+            var bytes = new byte[2048];
+
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                file.Read(bytes, 0, bytes.Length);
+            }
+
+            var headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
+            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return dt.AddSeconds(secondsSince1970);
+        }
+    }
+}
---
+++ Librarian.Sephirah/Services/Internal/GetServerInformation.cs
@@ -0,0 +1,29 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Librarian.Common.Utils;
+using System.Reflection;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        public override Task<GetServerInformationResponse> GetServerInformation(GetServerInformationRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetServerInformationResponse
+            {
+                ServerBinarySummary = new ServerBinarySummary
+                {
+                    SourceCodeAddress = "https://github.com/tuihub/Librarian-CSharp",
+                    BuildVersion = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "Unknown",
+                    BuildDate = LinkerTimestampUtil.GetLinkerTimestamp(Assembly.GetExecutingAssembly()).ToISO8601String()
+                },
+                ProtocolSummary = new ServerProtocolSummary
+                {
+                    Version = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "Unknown",
+                },
+                CurrentTime = DateTime.UtcNow.ToTimestamp()
+            });
+        }
+    }
+}
