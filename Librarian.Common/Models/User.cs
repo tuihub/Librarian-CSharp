fa49468d6403dde28276743f9916162fc749be0b
Tabing010102 <tabing010102@outlook.com>
2023-10-19 12:46:02 +0800

    feat(Tiphereth): impl GetUser

--- Librarian.Common/Models/User.cs
+++ Librarian.Common/Models/User.cs
@@ -30,5 +30,21 @@ namespace Librarian.Common.Models
         public ICollection<UserAppAppCategory> UserAppAppCategories { get; } = new List<UserAppAppCategory>();
         // many-to-many relation(wihtout entity, to other parent)
         public ICollection<App> Apps { get; } = new List<App>();
+
+        public TuiHub.Protos.Librarian.Sephirah.V1.User ToProtoUser(bool withPassword = false)
+        {
+            var ret = new TuiHub.Protos.Librarian.Sephirah.V1.User
+            {
+                Id = new TuiHub.Protos.Librarian.V1.InternalID { Id = Id },
+                Username = Name,
+                Type = Type,
+                Status = Status
+            };
+            if (withPassword)
+            {
+                ret.Password = Password;
+            }
+            return ret;
+        }
     }
 }
---
+++ Librarian.Sephirah/Services/Tiphereth/GetUser.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Librarian.Common.Utils;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+
+namespace Librarian.Sephirah.Services
+{
+    public partial class SephirahService : LibrarianSephirahService.LibrarianSephirahServiceBase
+    {
+        public override Task<GetUserResponse> GetUser(GetUserRequest request, ServerCallContext context)
+        {
+            var userId = JwtUtil.GetInternalIdFromJwt(context);
+            // NOTE: request id can be null
+            if (request.Id?.Id != null && request.Id.Id != userId)
+            {
+                // verify user type(admin)
+                if (UserUtil.GetUserTypeFromJwt(context, _dbContext) != UserType.Admin)
+                    throw new RpcException(new Status(StatusCode.PermissionDenied, "Access Deined."));
+            }
+            // get user
+            var userIdToGet = request.Id?.Id == null ? userId : request.Id.Id;
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == userIdToGet);
+            if (user == null)
+                throw new RpcException(new Status(StatusCode.PermissionDenied, "User not exists."));
+            return Task.FromResult(new GetUserResponse
+            {
+                User = user.ToProtoUser()
+            });
+        }
+    }
+}
