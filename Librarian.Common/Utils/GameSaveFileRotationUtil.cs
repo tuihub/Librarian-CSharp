bb0506dbbea5cfa37b7dcf53c430fee0e93ff3f8
Tabing010102 <tabing010102@outlook.com>
2023-10-22 19:11:24 +0800

    fix: update GetGameSaveFileRotation

--- Librarian.Common/Utils/GameSaveFileRotationUtil.cs
+++ Librarian.Common/Utils/GameSaveFileRotationUtil.cs
@@ -14,17 +14,19 @@
         public static long? GetGameSaveFileRotation(ApplicationDbContext db, long userId, long appPackageId)
         {
             // AppPackage
-            var ret = db.GameSaveFileRotations.SingleOrDefault(x => x.EntityInternalId == appPackageId
-                                                                && x.VaildScope == Models.VaildScope.AppPackage)?.Count;
+            var ret = db.GameSaveFileRotations.SingleOrDefault(x => x.UserId == userId &&
+                                                                    x.EntityInternalId == appPackageId &&
+                                                                    x.VaildScope == Models.VaildScope.AppPackage)?.Count;
             if (ret != null) return ret;
             // App
             var appId = db.AppPackages.Single(x => x.Id == appPackageId).AppId;
-            ret = db.GameSaveFileRotations.SingleOrDefault(x => x.EntityInternalId == appId
-                                                            && x.VaildScope == Models.VaildScope.App)?.Count;
+            ret = db.GameSaveFileRotations.SingleOrDefault(x => x.UserId == userId &&
+                                                                x.EntityInternalId == appId &&
+                                                                x.VaildScope == Models.VaildScope.App)?.Count;
             if (ret != null) return ret;
             // User
-            ret = db.GameSaveFileRotations.SingleOrDefault(x => x.EntityInternalId == userId
-                                                            && x.VaildScope == Models.VaildScope.Account)?.Count;
+            ret = db.GameSaveFileRotations.SingleOrDefault(x => x.UserId == userId &&
+                                                                x.VaildScope == Models.VaildScope.Account)?.Count;
             return ret;
         }
     }
