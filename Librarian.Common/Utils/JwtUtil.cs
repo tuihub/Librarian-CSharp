527697abbc80d41a4a2fc8a2a730c7ee79dceb46
Tabing010102 <tabing010102@outlook.com>
2024-02-28 22:52:50 +0800

    fix(Common.Utils): fix JwtUtil generate upload/download token

--- Librarian.Common/Utils/JwtUtil.cs
+++ Librarian.Common/Utils/JwtUtil.cs
@@ -54,14 +54,14 @@ namespace Librarian.Common.Utils
         }
         public static string GenerateUploadToken(long internalId)
         {
-            var expireMinutes = GlobalContext.JwtConfig.AccessTokenExpireMinutes;
+            var expireMinutes = GlobalContext.JwtConfig.UploadTokenExpireMinutes;
             var audience = GlobalContext.JwtConfig.UploadTokenAudience;
             var issuer = GlobalContext.JwtConfig.Issuer;
             return GenerateToken(issuer, audience, expireMinutes, internalId);
         }
         public static string GenerateDownloadToken(long internalId)
         {
-            var expireMinutes = GlobalContext.JwtConfig.AccessTokenExpireMinutes;
+            var expireMinutes = GlobalContext.JwtConfig.DownloadTokenExpireMinutes;
             var audience = GlobalContext.JwtConfig.DownloadTokenAudience;
             var issuer = GlobalContext.JwtConfig.Issuer;
             return GenerateToken(issuer, audience, expireMinutes, internalId);
