4c0fb80250b22d2e5310c6227d28059f1208cf27
Tabing010102 <tabing010102@outlook.com>
2024-10-23 16:31:12 +0800

    fix: check key in porter config

--- Librarian.Porter.Server/Utils/ServicesUtil.cs
+++ Librarian.Porter.Server/Utils/ServicesUtil.cs
@@ -4,22 +4,37 @@ using Librarian.ThirdParty.Bangumi;
 using Librarian.ThirdParty.Contracts;
 using Librarian.ThirdParty.Steam;
 using Librarian.ThirdParty.Vndb;
+using Microsoft.Extensions.Logging;
 
 namespace Librarian.Porter.Server.Utils
 {
     public static class ServicesUtil
     {
-        public static void ConfigureThirdPartyServices(WebApplicationBuilder builder, PorterConfig porterConfig)
+        public static void ConfigureThirdPartyServices(WebApplicationBuilder builder, PorterConfig porterConfig, ILogger? logger)
         {
             if (porterConfig.IsSteamEnabled)
             {
-                builder.Services.AddSingleton(new SteamAPIService(porterConfig.SteamApiKey));
-                StaticContext.PorterTags.Add(WellKnownAppInfoSource.Steam);
+                if (string.IsNullOrEmpty(porterConfig.SteamApiKey))
+                {
+                    logger?.LogError("Steam API key is not provided.");
+                }
+                else
+                {
+                    builder.Services.AddSingleton(new SteamAPIService(porterConfig.SteamApiKey));
+                    StaticContext.PorterTags.Add(WellKnownAppInfoSource.Steam);
+                }
             }
             if (porterConfig.IsBangumiEnabled)
             {
-                builder.Services.AddSingleton(new BangumiAPIService(porterConfig.BangumiApiKey));
-                StaticContext.PorterTags.Add(WellKnownAppInfoSource.Bangumi);
+                if (string.IsNullOrEmpty(porterConfig.BangumiApiKey))
+                {
+                    logger?.LogError("Bangumi API key is not provided.");
+                }
+                else
+                {
+                    builder.Services.AddSingleton(new BangumiAPIService(porterConfig.BangumiApiKey));
+                    StaticContext.PorterTags.Add(WellKnownAppInfoSource.Bangumi);
+                }
             }
             if (porterConfig.IsVndbEnabled)
             {
