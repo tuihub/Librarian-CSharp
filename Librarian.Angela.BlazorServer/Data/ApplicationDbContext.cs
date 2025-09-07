11382a30803e2c3c099bba9e43756c44c18999cb
Tabing010102 <tabing010102@outlook.com>
2025-09-08 00:12:40 +0800

    chore: update BlazorServer

--- Librarian.Angela.BlazorServer/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Librarian.Angela.BlazorServer.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : IdentityDbContext<ApplicationUser>(options)
-{
-}
